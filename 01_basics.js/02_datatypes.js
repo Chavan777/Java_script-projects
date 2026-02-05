71fde4aeb53122e37d05606767d6d3ef60b19b18
Chavan777 <136989225+Chavan777@users.noreply.github.com>
2026-02-05 16:28:50 +0000

    datatypes

---
+++ 01_basics.js/02_datatypes.js
@@ -0,0 +1,15 @@
+let name = "Chavan"
+let age = 18;
+let isLoggedIn = false;
+
+//number => 2 to power 53
+// bigint => for big numbers
+// Strings => " "
+// Boolean = True/false
+// null => Null type has exactly one value, or stand alone value
+// undefined =>
+// symbol => unique
+// null => while running it shows in terminal as objects
+// undefinded => where while running in terminal a undefined value it show undefined
+console.log(typeof "Chavan");
+console.log(typeof null);
