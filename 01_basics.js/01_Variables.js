53cbcecebc5a6fd99024b17f6df6f24f1006bec5
Chavan777 <136989225+Chavan777@users.noreply.github.com>
2026-02-05 15:57:33 +0000

    Variables

---
+++ 01_basics.js/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 123456;
+let accountEmail = "nitinhc@gmail.com";
+var accountPassword = "12345";
+accountCity = "New York";
+let accountState;
+
+//let accountState;
+
+accountEmail = "nc@gmail.com";
+//accountId =2;
+accountPassword = "212121"
+accountCity  = "Bengaluru";
+
+console.log([accountId, accountEmail, accountPassword, accountCity, accountState]);
